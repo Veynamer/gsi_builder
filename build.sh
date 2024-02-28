48d36189a3ce8550b21dfb9b4768ec41ddb1f980
Veynamer <113140575+Veynamer@users.noreply.github.com>
2024-02-28 14:28:30 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Set Crave to build using LineageOS 21 as base
-repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
+#repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 crave set --projectID 73
 
 # Run inside foss.crave.io devspace, in the project folder
